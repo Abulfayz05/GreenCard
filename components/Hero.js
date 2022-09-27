78a2db03b969c4496c5cdc1c579113d7883ddd89
Abulfayz <abulfayzmamatqulov@gmail.com>
2022-09-28 00:54:08 +0500

    container hero

--- components/Hero.js
+++ components/Hero.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import styles from "../styles/Hero.module.css"
+import React from "react";
+import styles from "../styles/Hero.module.css";
 export default function Hero() {
   return (
     <div className={styles.hero}>
-        
-       <div className={styles.herocontent}>
-        <h2>Greenkarta.uz bilan Green Card lotereyasini yutib oling</h2>
-        <h3>Biz yil davomida anketalarni qabul qilamiz</h3>
-       </div>
-        
+      <div className={styles.herocontent}>
+        <div className={styles.container_page}>
+          <h2>Greenkarta.uz bilan Green Card lotereyasini yutib oling</h2>
+          <h3>Biz yil davomida anketalarni qabul qilamiz</h3>
         </div>
-  )
+      </div>
+    </div>
+  );
 }
