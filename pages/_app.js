048067d4a728cd98ace9557ddae2ff43f5704566
Abulfayz <abulfayzmamatqulov@gmail.com>
2022-10-03 23:18:38 +0500

    yandex verification

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,7 @@ function MyApp({ Component, pageProps }) {
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
         />
+        <meta name="yandex-verification" content="55114251dfe82623" />
         <link rel="icon" href="/logo.png" />
       </Head>
       <Gtag />
