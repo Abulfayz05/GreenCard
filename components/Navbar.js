5c06b1a61e3b242b45e95b946c25fe9c7793731f
Abulfayz <abulfayzmamatqulov@gmail.com>
2022-10-07 01:22:54 +0500

    menu, hero news responsive

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,13 @@
 import React from "react";
 import Link from "next/link";
 import styles from "../styles/Navbar.module.css";
+import { useState } from "react";
+import { VscMenu } from "react-icons/vsc";
+import { VscChromeClose } from "react-icons/vsc";
 export default function Navbar() {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
+  console.log(isNavExpanded);
   return (
     <div className={styles.header}>
       <div className={styles.container}>
@@ -17,8 +23,29 @@ export default function Navbar() {
           </Link>
         </div>
 
-        <nav className={styles.menu}>
-          <ul>
+        <button
+          className={styles.hamburger}
+          onClick={() => {
+            setIsNavExpanded(!isNavExpanded);
+          }}
+        >
+          <VscMenu size={30} color="white" />
+        </button>
+
+        <nav className={isNavExpanded ? styles.menu_active : styles.menu}>
+          <button
+            className={styles.hamburger_close}
+            onClick={() => {
+              setIsNavExpanded(!isNavExpanded);
+            }}
+          >
+            <VscChromeClose size={30} />
+          </button>
+          <ul
+            onClick={() => {
+              setIsNavExpanded(!isNavExpanded);
+            }}
+          >
             <li>
               <Link href="/">
                 <a>Bosh sahifa</a>
@@ -30,7 +57,7 @@ export default function Navbar() {
                 <a>Qo&apos;llanma</a>
               </Link>
             </li>
-        
+
             <li>
               <Link href="/news">
                 <a>Yangiliklar</a>
