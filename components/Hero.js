c592f7b3f1760ac314dca358499234599f696a88
Abulfayz <abulfayzmamatqulov@gmail.com>
2022-11-06 16:15:25 +0500

    bug fixed

--- components/Hero.js
+++ components/Hero.js
@@ -6,6 +6,7 @@ export default function Hero() {
       <div className={styles.container_page}>
         <div className={styles.herocontent}>
           <h2>Greenkarta.uz bilan Green Card lotereyasini yutib oling</h2>
+          <h3>(97) 435 05 12</h3>
         </div>
       </div>
     </div>
